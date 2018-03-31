751780a49bea3fd34ab07c90455c573766ecff57
Nabil Daoud <nabiljdaoud@gmail.com>
2018-03-31 22:26:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,3 +27,11 @@ function appendKitten(name) {
 function prependKitten(name) {
   return [name, ...kittens];
 }
+
+function removeLastKitten() {
+  return kittens.slice(0,-1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
