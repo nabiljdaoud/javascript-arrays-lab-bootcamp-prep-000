60461dabb682ae99e45f169e7c0791c77dc56ab3
Nabil Daoud <nabiljdaoud@gmail.com>
2018-03-31 22:23:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  return [kittens, name];
+  return [...kittens, name];
 }
 
 function prependKitten(name) {
-  
+  return [name, ...kittens];
 }
