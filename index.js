758589c82b52d8850823a54efbd2571f294a8aa9
Nabil Daoud <nabiljdaoud@gmail.com>
2018-03-31 22:15:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  return kittens.append(name);
+}
